46391a34c6455d59bfb4e4e7a7d4880466d57e0a
noiseWizard <64124242+noiseWizard@users.noreply.github.com>
2024-07-05 16:45:30 +1000

    fixed, added comment.
    issue was with the " brackets not matching.

--- 1. C for Everyone: Programming Fundamentals/1. Fix Mr P's mistake (week 1) (fix helloworld).c
+++ 1. C for Everyone: Programming Fundamentals/1. Fix Mr P's mistake (week 1) (fix helloworld).c
@@ -1,6 +1,12 @@
+/*
+Fixing Dr P's Mistake, week 1 lesson
+[name]
+5 July 2024
+*/
+
 #include<stdio.h>
 int main(void)
 {   
-    printf(‘ hello world\n’);
+    printf("hello world\n");
     return 0;
 }    
