80c5521dd930865df9cd6b878eb7e46365427ab3
noiseWizard <64124242+noiseWizard@users.noreply.github.com>
2024-07-14 10:44:07 +1000

    first attempt

---
+++ 1. C for Everyone: Programming Fundamentals/0. additional projects/1. Valentines Day program, using for loops.c
@@ -0,0 +1,19 @@
+/*
+    attempt at turning the 3.5 example of a while loop into a for loop.
+    14 July 2024
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int repeat = 0;
+    int i;
+    printf("Guess how much I love you. (1-10):");
+    scanf("d%, &repeat");
+    printf("\nI love you very");
+    for (i = 0; i <= repeat; i++){
+        printf("\n very");
+    };
+    printf(" much.\n\n");
+    return 0;
+}
