c1425b8a9848c3d163a44c1fe515ae79f1e363ac
noiseWizard <64124242+noiseWizard@users.noreply.github.com>
2024-07-22 19:12:36 +1000

    creating the initial based on learning examples

--- 1. C for Everyone: Programming Fundamentals/7. average weight elephant seals.c
+++ 1. C for Everyone: Programming Fundamentals/7. average weight elephant seals.c
@@ -4,3 +4,34 @@
     July 2024
 */
 
+#include <stdio.h>
+
+double average_weight(int how_many, int weight[]){
+    int i;
+    double sum = 0.0;
+
+    for (i = 0; i < how_many; i++){
+        sum += weight[i];
+    }
+    return(sum/weight);
+}
+
+void print_weight(int how_many, int weight[]){
+    int i;
+
+    print("Total data size: %d", how_many);
+    for (i = 0; i < how_many; i++){
+        printf("%d\t", weight[i]);
+    }
+}
+
+int main(void){
+    const int arraySize;
+    int weight[arraySize];
+
+    print_grades(arraySize, weight);
+    print("\n\n");
+    printf("the average is %.2f\n\n", average_weight(arraySize, weight));
+
+    return 0;
+}
